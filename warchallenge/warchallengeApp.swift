3fd4060c95237f178defecc038f3a24eab5742e3
EllieNguyen <ellieN1235@gmail.com>
2023-02-05 04:13:46 -0800

    Initial Commit

---
+++ warchallenge/warchallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  warchallengeApp.swift
+//  warchallenge
+//
+//  Created by Ellie on 2/5/23.
+//
+
+import SwiftUI
+
+@main
+struct warchallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
